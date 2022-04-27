450b2dc1d5c5d4ebe5ff2e2131f4c803c8d17dd8
jaypassley <jaypassley@yahoo.com>
2022-04-27 00:22:27 -0500

    form creation

--- src/com/passleyart/Currency_Converter/Converter.java
+++ src/com/passleyart/Currency_Converter/Converter.java
@@ -1,4 +1,11 @@
 package com.passleyart.Currency_Converter;
 
+import javax.swing.*;
+
 public class Converter {
+    private JButton convertButton;
+    private JButton clearButton;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JTextField textField2;
 }
